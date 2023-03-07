819284bd391346e6607e0477f7f1c2958d343a92
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-07 13:39:02 -0500

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -4,4 +4,44 @@ const sequelize = require("../config/connection");
 // Creates a new Sequelize model for Comments
 class Comment extends Model {}
 
+Comment.init(
+  // Defines fields/columns on model
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    // Comment content
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "blog",
+        key: "id",
+      },
+    },
+  },
+  {
+    // Links to database connection
+    sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
+    // timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
 module.exports = Comment;
