2614986268e22a988b5d80384556b4f947066e50
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-07 13:38:59 -0500

    Update Blog.js

--- models/Blog.js
+++ models/Blog.js
@@ -4,4 +4,48 @@ const sequelize = require("../config/connection");
 // Creates a new Sequelize model for Blogs
 class Blog extends Model {}
 
+Blog.init(
+  // Defines fields/columns on model
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    // Blog post title
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Blog post date created
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    // Blog post content
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    // Links to database connection
+    sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
+    // timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "blog",
+  }
+);
+
 module.exports = Blog;
