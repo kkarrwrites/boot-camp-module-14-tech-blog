84f9b960dd83b275f42fe94a72e128f541f7be70
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-07 11:37:49 -0500

    Add comments

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,18 @@
 const Sequelize = require("sequelize");
+// Enable access to .env variables
 require("dotenv").config();
 
 let sequelize;
-
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
+  // Use environment variables to connect to database
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
+      // Database location
       host: process.env.DB_HOST,
       dialect: "mysql",
       port: 3306,
