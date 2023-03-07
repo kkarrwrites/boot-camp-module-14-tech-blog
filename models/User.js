26a9fb5f9e785daa58923827a6b79a6c44da5fd4
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-07 12:41:45 -0500

    Update User.js

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+const bcrypt = require("bcrypt");
+
+// Create a new Sequelize model for Users
+class User extends Model {
+  checkPassword(loginPassword) {
+    return bcrypt.compareSync(loginPassword, this.password);
+  }
+}
+
+module.exports = User;
