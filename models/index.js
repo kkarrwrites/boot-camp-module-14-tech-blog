2deccc57eceb997a0ebe5acbbce3e2047f64a759
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-08 07:59:48 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -11,6 +11,7 @@ User.hasMany(Blog, {
 // The association between a User and Blogs can also be created from the Blog side
 Blog.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // Defines a User as having many Comments, thus creating a foreign key in the `comment` table
@@ -22,16 +23,19 @@ User.hasMany(Comment, {
 // The association between a User and Comments can also be created from the Comment side
 Comment.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 // Defines a Blog as having many Comments, thus creating a foreign key in the `comment` table
 Blog.hasMany(Comment, {
   foreignKey: "post_id",
+  onDelete: "CASCADE",
 });
 
 // The association between Blog and Comments can also be created from the Comment side
 Comment.belongsTo(Blog, {
   foreignKey: "post_id",
+  onDelete: "CASCADE",
 });
 
 // Packages the models and exports them as an object so we can import them together and use their proper names
