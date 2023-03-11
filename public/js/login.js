718ecbc2294e3081a34bc9991f268771034a1cfd
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-11 14:35:54 -0500

    Create login.js
    Create loginFormHandler function

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Declare variables and collect values from the login form
+  const username = document.querySelector("#username_login").value.trim();
+  const password = document.querySelector("#password_login").value.trim();
+
+  if (username && password) {
+    // POST (Read) request sent to the API endpoint
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the brower to the Dashboard page
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// Add event listener to the login form submit
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
