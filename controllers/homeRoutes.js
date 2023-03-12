02cc9dfa767164b8de168a3e12e575484ee56587
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-11 19:22:15 -0500

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,7 +48,8 @@ router.get("/dashboard", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ["password"], include: [{ model: Blog }] },
+      attributes: { exclude: ["password"] },
+      include: [{ model: Blog }],
     });
 
     const user = userData.get({ plain: true });
@@ -63,17 +64,14 @@ router.get("/dashboard", withAuth, async (req, res) => {
 });
 
 // GET (Read) for Log In
-router.get("/login", async (req, res) => {
-  // If the user is already logged in, redirect the request to the Dashboard
-  try {
-    if (req.session.logged_in) {
-      res.redirect("/dashboard");
-      return;
-    }
-    res.render("login");
-  } catch (err) {
-    res.status(500).json(err);
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
   }
+
+  res.render("login");
 });
 
 // GET (Read) for Sign Up
