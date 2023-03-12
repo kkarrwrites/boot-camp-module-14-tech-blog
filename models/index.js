53a067d2ff043c91fc851fe7a7c18dc315145824
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-11 19:22:39 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,6 +5,11 @@ const Comment = require("./Comment");
 // Defines a User as having many Blogs, thus creating a foreign key in the `blog` table
 User.hasMany(Blog, {
   foreignKey: "user_id",
+});
+
+// Defines a User as having many Comments, thus creating a foreign key in the `comment` table
+User.hasMany(Comment, {
+  foreignKey: "user_id",
   onDelete: "CASCADE",
 });
 
@@ -14,21 +19,15 @@ Blog.belongsTo(User, {
   onDelete: "CASCADE",
 });
 
-// Defines a User as having many Comments, thus creating a foreign key in the `comment` table
-User.hasMany(Comment, {
-  foreignKey: "user_id",
+// Defines a Blog as having many Comments, thus creating a foreign key in the `comment` table
+Blog.hasMany(Comment, {
+  foreignKey: "blog_id",
   onDelete: "CASCADE",
 });
 
 // The association between a User and Comments can also be created from the Comment side
 Comment.belongsTo(User, {
   foreignKey: "user_id",
-  onDelete: "CASCADE",
-});
-
-// Defines a Blog as having many Comments, thus creating a foreign key in the `comment` table
-Blog.hasMany(Comment, {
-  foreignKey: "blog_id",
   onDelete: "CASCADE",
 });
 
