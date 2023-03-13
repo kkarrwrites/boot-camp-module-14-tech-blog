01ff4978000b5795812c6df662dcf22a7d83d0a5
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-13 16:07:33 -0400

    Update logout.js
    Add comments to logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,17 @@
 const logout = async () => {
+  // POST (Read) request sent to the API endpoint
   const response = await fetch("/api/users/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
+    // If successful, redirect the brower to the home page
     document.location.replace("/");
   } else {
     alert(response.statusText);
   }
 };
 
+// Add event listener to the logout form submit
 document.querySelector("#logout").addEventListener("click", logout);
