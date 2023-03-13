4717f7a12e84ad85da627cf4524a44d3d4c59068
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-13 12:45:19 -0400

    Update homeRoutes.js
    Change signup router

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -75,12 +75,14 @@ router.get("/login", (req, res) => {
 });
 
 // GET (Read) for Sign Up
-router.get("/signup", async (req, res) => {
-  try {
-    res.render("signup");
-  } catch (err) {
-    res.status(500).json(err);
+router.get("/signup", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
   }
+
+  res.render("signup");
 });
 
 module.exports = router;
