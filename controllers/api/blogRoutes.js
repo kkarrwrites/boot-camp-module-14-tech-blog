3cf17af9ba7bf32d056088d7ebcccd686b812230
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-14 14:09:02 -0400

    Create blogRoutes.js

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const { Comment, User, Blog } = require("../../models");
+const withAuth = require("../../utils/withAuth");
+
+// POST (Create) new blog route
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newBlog = await Blog.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newBlog);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
