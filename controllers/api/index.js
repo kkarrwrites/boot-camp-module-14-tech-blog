fdfc96413f4cb5a0a23103d22b946a12517625d8
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-14 15:21:56 -0400

    Add editRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const commentRoutes = require("./commentRoutes");
 const blogRoutes = require("./blogRoutes");
+const editRoutes = require("./editRoutes");
 
 router.use("/users", userRoutes);
 router.use("/comments", commentRoutes);
 router.use("/blogs", blogRoutes);
+router.use("/edit", editRoutes);
 
 module.exports = router;
