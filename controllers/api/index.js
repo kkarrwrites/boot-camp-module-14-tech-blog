5255f02172dbfe86f3ee00b935f07b3f99b88dd8
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-03-14 14:08:57 -0400

    Add blogRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./userRoutes");
 const commentRoutes = require("./commentRoutes");
+const blogRoutes = require("./blogRoutes");
 
 router.use("/users", userRoutes);
 router.use("/comments", commentRoutes);
+router.use("/blogs", blogRoutes);
 
 module.exports = router;
